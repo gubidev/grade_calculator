21249b8e628c1f351c4a9c90fd99b1cb53e932ac
Felipe Caiafa <felipe.soares@sou.inteli.edu.br>
2025-02-10 11:32:33 -0300

    Add files via upload

---
+++ Atividade/notas.js
@@ -0,0 +1,30 @@
+var nota1 = 6
+var nota2 = 9
+var nota3 = 9
+
+var media = (nota1 + nota2 + nota3 ) / 3
+
+if (media >=7){
+    console.log('Você foi Aprovado')
+}else{
+    console.log('Você foi Reprovado')
+}
+
+
+
+while (media < 7){
+    console.log('Nota 1: ' + nota1)
+    console.log('Nota 2: ' + nota2)
+    console.log('Nota 3: ' + nota3)
+    console.log('Media:' + media)
+ break
+}
+
+while (media >= 7){
+    console.log('Nota 1: ' + nota1)
+    console.log('Nota 2: ' + nota2)
+    console.log('Nota 3: ' + nota3)
+    console.log('Media:' + media)
+    break
+}
+
